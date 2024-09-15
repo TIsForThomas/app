f14a96fd995273e58004e6bc59c8fa237a1ac18f
Chris Johnson <cjohnson1519029@gmail.com>
2024-09-15 14:24:10 -0400

    add_usar imput

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from raid_values import *
 from entity_health import *
 
-print(garageDoor)
+#Getting input from user
+
+User_wants = input("What kind of item are you looking to break: ")
+if User_wants = garageDoor Print yea
+User_how_many = input ("How many of this item do you need to break: ")
+User_raid_type = input ("What are you going to be raiding with: ")
+
+
+
+
+
